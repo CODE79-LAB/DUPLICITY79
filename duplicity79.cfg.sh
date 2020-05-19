d66fb8b199025cce9405cba81756a78653643d55
Gilles Fauvie <1789515+gfauvie@users.noreply.github.com>
2020-05-19 22:23:42 +0200

    relase found on internet
    relase found on internet

---
+++ duplicity79.cfg.sh
@@ -0,0 +1,10 @@
+############################################################
+# CODE79.com
+# duplicity79
+############################################################
+
+# Put this file in your root directory for security reason
+
+export AWS_ACCESS_KEY_ID="AAAAAAAAAAAAAAA"
+export AWS_SECRET_ACCESS_KEY="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+export PASSPHRASE=""
