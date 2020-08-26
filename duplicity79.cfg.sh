fe9daa2c0e6c50692b52a05917650ce7390973d1
Gilles Fauvie <1789515+gfauvie@users.noreply.github.com>
2020-08-27 00:36:41 +0200

    Update duplicity79.cfg.sh

--- duplicity79.cfg.sh
+++ duplicity79.cfg.sh
@@ -13,4 +13,5 @@ export PASSPHRASE=""
 export GPG_KEY=""
 
 # The S3 destination followed by bucket name
-export DEST="s3://infra.code79.backup/$HOSTNAME/"
+$host=`hostname -f`
+export DEST="s3://infra.code79.backup/$host/"
